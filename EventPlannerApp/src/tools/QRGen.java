af97335657b1fa281be2dd90877e9c2016faf9bd
Moimus <33897774+Moimus@users.noreply.github.com>
2019-10-16 18:41:22 +0200

    added basic project structure
    Added basic project, ZXing QR generator & simpleJSON

---
+++ EventPlannerApp/src/tools/QRGen.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Hendrik
+ */
+public class QRGen 
+{
+    /**
+     * generates a qr code from a string
+     * @param Content
+     * @return QRCode
+     */
+    	public static BufferedImage generateQRCode(String Content)
+	{
+		BufferedImage out = null;
+		
+		try
+		{
+                    QRCodeWriter writer = new QRCodeWriter();
+                    BitMatrix matrix = writer.encode(Content, BarcodeFormat.QR_CODE, 512, 512);
+                    out = MatrixToImageWriter.toBufferedImage(matrix);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+		return out;
+	}
+}
---
+++ EventPlannerApp/src/viewmodel/SampleViewModel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viewmodel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class SampleViewModel implements Initializable 
+{
+
+    @FXML
+    private Label label;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        label.setText("programmatically assigned text");
+    }    
+    
+}
